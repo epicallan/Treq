78818cc0585d171aa465d540074e2e14bbcfeee0
allan <epicallan.al@gmail.com>
2019-10-30 13:11:12 +0300

    feat: add tests and minor refactoring

---
+++ test/Hreq/FailSpec.hs
@@ -0,0 +1,26 @@
+module Hreq.FailSpec (spec) where
+
+import Hreq.Util (TestState (..), TestUser, defaultResponse, runHttpPure)
+import Network.HTTP.Hreq
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Servant.FailSpec" $ do
+    failSpec
+
+failSpec :: Spec
+failSpec = do
+  let baseUrl = BaseUrl Http "example.com" 80 ""
+
+  describe "throw appropriate errors" $ do
+    it "throw failure error" $ do
+      let x = hreq @("hello" :> RawResponse GET) Empty
+          req' = appendToPath "hello" defaultRequest
+      runHttpPure @'FailureState baseUrl x
+        `shouldBe` Throw (FailureResponse req' defaultResponse)
+
+    it "throws decoding failure error" $ do
+      let x = hreq @(GetJson TestUser) Empty
+
+      show (runHttpPure @'DecodingErrorState baseUrl x)
+        `shouldContain` "DecodeFailure"

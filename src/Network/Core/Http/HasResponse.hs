6236a1ecd4e8dfbc5f501c4a81080e3bc6f3908f
allan <epicallan.al@gmail.com>
2019-10-31 22:35:11 +0300

    chore: add more docs and minor refactors

--- src/Network/Core/Http/HasResponse.hs
+++ src/Network/Core/Http/HasResponse.hs
@@ -9,7 +9,6 @@ import GHC.TypeLits
 import Data.Proxy
 import Network.HTTP.Types (hContentType)
 import Data.ByteString.Lazy as LBS
-import Network.HTTP.Media (parseAccept, (//), matches)
 
 import Network.Core.API
 import Network.Core.Http.Hlist
@@ -98,7 +97,7 @@ decodeAsBody _ response = do
   unless (responseContentType `matches` accept)
     . throwError $ UnsupportedContentType accept response
 
-  case decode ctypProxy (LBS.toStrict $ resBody response) of
+  case mediaDecode ctypProxy (LBS.toStrict $ resBody response) of
      Left err -> throwError $ DecodeFailure (unDecodeError err) response
      Right val -> pure val
   where
@@ -110,7 +109,7 @@ decodeAsBody _ response = do
 
     checkContentType :: m MediaType
     checkContentType  = case lookup hContentType $ resHeaders response of
-      Nothing -> return $ "application"//"octet-stream" -- | TODO: implement streaming
+      Nothing -> return $ mediaType (Proxy @PlainText) -- fall back to plain text
       Just t  -> maybe (throwError $ InvalidContentTypeHeader response) return $ parseAccept t
 
 decodeAsHlist
@@ -134,7 +133,7 @@ decodeAsHlist srs response = case srs of
   SCons (SResHeaders SNil) xs ->
     decodeAsHlist xs response
 
-  (SCons (SRaw _) _xs)-> error "GHC Error"
-  -- ^ Should never match because we have a class instance
+  -- Should never match because we have a class instance
   -- that triggers a type error when 'Raw' is in a non-singleton
   -- type level list
+  (SCons (SRaw _) _xs)-> error "GHC Error"
